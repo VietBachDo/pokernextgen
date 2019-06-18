391c006f309fe2075cae690066ee225245c7f0b4
Viet Bach Do <vietbachdo@wustl.edu>
2019-06-18 01:55:48 -0500

    done with deselect

--- src/assets/js/simulator.js
+++ src/assets/js/simulator.js
@@ -1,66 +1,68 @@
 var dict = {};
-dict['1'] = ['<span class="selected-cards" onClick="print_id(1)">&#127186;</span>', false];
-dict['2'] = ['<span class="selected-cards" style="color: red; ">&#127170;</span>', false];
-dict['3'] = ['<span class="selected-cards" style="color: red; ">&#127154;</span>', false];
-dict['4'] = ['<span class="selected-cards" >&#127138;</span>', false];
-dict['5'] = ['<span class="selected-cards" >&#127187;</span>', false];
-dict['6'] = ['<span class="selected-cards" style="color: red; ">&#127171;</span>', false];
-dict['7'] = ['<span class="selected-cards" style="color: red; ">&#127155;</span>', false];
-dict['8'] = ['<span class="selected-cards" >&#127139;</span>', false];
-dict['9'] = ['<span class="selected-cards" >&#127188;</span>', false];
-dict['10'] = ['<span class="selected-cards" style="color: red; ">&#127172;</span>', false];
-dict['11'] = ['<span class="selected-cards" style="color: red; ">&#127156;</span>', false];
-dict['12'] = ['<span class="selected-cards" >&#127140;</span>', false];
-dict['13'] = ['<span class="selected-cards" >&#127189;</span>', false];
-dict['14'] = ['<span class="selected-cards" style="color: red; ">&#127173;</span>', false];
-dict['15'] = ['<span class="selected-cards" style="color: red; ">&#127157;</span>', false];
-dict['16'] = ['<span class="selected-cards" >&#127141;</span>', false];
-dict['17'] = ['<span class="selected-cards" >&#127190;</span>', false];
-dict['18'] = ['<span class="selected-cards" style="color: red; ">&#127174;</span>', false];
-dict['19'] = ['<span class="selected-cards" style="color: red; ">&#127158;</span>', false];
-dict['20'] = ['<span class="selected-cards" >&#127142;</span>', false];
-dict['21'] = ['<span class="selected-cards" >&#127191;</span>', false];
-dict['22'] = ['<span class="selected-cards" style="color: red; ">&#127175;</span>', false];
-dict['23'] = ['<span class="selected-cards" style="color: red; ">&#127159;</span>', false];
-dict['24'] = ['<span class="selected-cards" >&#127143;</span>', false];
-dict['25'] = ['<span class="selected-cards" >&#127192;</span>', false];
-dict['26'] = ['<span class="selected-cards" style="color: red; ">&#127176;</span>', false];
-dict['27'] = ['<span class="selected-cards" style="color: red; ">&#127160;</span>', false];
-dict['28'] = ['<span class="selected-cards" >&#127144;</span>', false];
-dict['29'] = ['<span class="selected-cards" >&#127193;</span>', false];
-dict['30'] = ['<span class="selected-cards" style="color: red; ">&#127177;</span>', false];
-dict['31'] = ['<span class="selected-cards" style="color: red; ">&#127161;</span>', false];
-dict['32'] = ['<span class="selected-cards" >&#127145;</span>', false];
-dict['33'] = ['<span class="selected-cards" >&#127194;</span>', false];
-dict['34'] = ['<span class="selected-cards" style="color: red; ">&#127178;</span>', false];
-dict['35'] = ['<span class="selected-cards" style="color: red; ">&#127162;</span>', false];
-dict['36'] = ['<span class="selected-cards" >&#127146;</span>', false];
-dict['37'] = ['<span class="selected-cards" >&#127195;</span>', false];
-dict['38'] = ['<span class="selected-cards" style="color: red; ">&#127179;</span>', false];
-dict['39'] = ['<span class="selected-cards" style="color: red; ">&#127163;</span>', false];
-dict['40'] = ['<span class="selected-cards" >&#127147;</span>', false];
-dict['41'] = ['<span class="selected-cards" >&#127197;</span>', false];
-dict['42'] = ['<span class="selected-cards" style="color: red; ">&#127181;</span>', false];
-dict['43'] = ['<span class="selected-cards" style="color: red; ">&#127165;</span>', false];
-dict['44'] = ['<span class="selected-cards" >&#127149;</span>', false];
-dict['45'] = ['<span class="selected-cards" >&#127198;</span>', false];
-dict['46'] = ['<span class="selected-cards" style="color: red; ">&#127182;</span>', false];
-dict['47'] = ['<span class="selected-cards" style="color: red; ">&#127166;</span>', false];
-dict['48'] = ['<span class="selected-cards" >&#127150;</span>', false];
-dict['49'] = ['<span class="selected-cards" >&#127185;</span>', false];
-dict['50'] = ['<span class="selected-cards" style="color: red; ">&#127169;</span>', false];
-dict['51'] = ['<span class="selected-cards" style="color: red; ">&#127153;</span>', false];
-dict['52'] = ['<span class="selected-cards" >&#127137;</span class="selected-cards">', false];
+dict['1'] = ['<img class="selected-cards" src="assets/imgs/cards/2_of_clubs.png" selected-card-id="1" onClick="print_id(1)">', false];
+dict['2'] = ['<img class="selected-cards" src="assets/imgs/cards/2_of_diamonds.png" selected-card-id="2" onClick="print_id(2)" style="color: red; ">', false];
+dict['3'] = ['<img class="selected-cards" src="assets/imgs/cards/2_of_hearts.png" selected-card-id="3" onClick="print_id(3)" style="color: red; ">', false];
+dict['4'] = ['<img class="selected-cards" src="assets/imgs/cards/2_of_spades.png" selected-card-id="4" onClick="print_id(4)">', false];
+dict['5'] = ['<img class="selected-cards" src="assets/imgs/cards/3_of_clubs.png" selected-card-id="5" onClick="print_id(5)">', false];
+dict['6'] = ['<img class="selected-cards" src="assets/imgs/cards/3_of_diamonds.png" selected-card-id="6" onClick="print_id(6)" style="color: red; ">', false];
+dict['7'] = ['<img class="selected-cards" src="assets/imgs/cards/3_of_hearts.png" selected-card-id="7" onClick="print_id(7)" style="color: red; ">', false];
+dict['8'] = ['<img class="selected-cards" src="assets/imgs/cards/3_of_spades.png" selected-card-id="8" onClick="print_id(8)">', false];
+dict['9'] = ['<img class="selected-cards" src="assets/imgs/cards/4_of_clubs.png" selected-card-id="9" onClick="print_id(9)">', false];
+dict['10'] = ['<img class="selected-cards" src="assets/imgs/cards/4_of_diamonds.png" selected-card-id="10" onClick="print_id(10)" style="color: red; ">', false];
+dict['11'] = ['<img class="selected-cards" src="assets/imgs/cards/4_of_hearts.png" selected-card-id="11" onClick="print_id(11)" style="color: red; ">', false];
+dict['12'] = ['<img class="selected-cards" src="assets/imgs/cards/4_of_spades.png" selected-card-id="12" onClick="print_id(12)">', false];
+dict['13'] = ['<img class="selected-cards" src="assets/imgs/cards/5_of_clubs.png" selected-card-id="13" onClick="print_id(13)">', false];
+dict['14'] = ['<img class="selected-cards" src="assets/imgs/cards/5_of_diamonds.png" selected-card-id="14" onClick="print_id(14)" style="color: red; ">', false];
+dict['15'] = ['<img class="selected-cards" src="assets/imgs/cards/5_of_hearts.png" selected-card-id="15" onClick="print_id(15)" style="color: red; ">', false];
+dict['16'] = ['<img class="selected-cards" src="assets/imgs/cards/5_of_spades.png" selected-card-id="16" onClick="print_id(16)">', false];
+dict['17'] = ['<img class="selected-cards" src="assets/imgs/cards/6_of_clubs.png" selected-card-id="17" onClick="print_id(17)">', false];
+dict['18'] = ['<img class="selected-cards" src="assets/imgs/cards/6_of_diamonds.png" selected-card-id="18" onClick="print_id(18)" style="color: red; ">', false];
+dict['19'] = ['<img class="selected-cards" src="assets/imgs/cards/6_of_hearts.png" selected-card-id="19" onClick="print_id(19)" style="color: red; ">', false];
+dict['20'] = ['<img class="selected-cards" src="assets/imgs/cards/6_of_spades.png" selected-card-id="20" onClick="print_id(20)">', false];
+dict['21'] = ['<img class="selected-cards" src="assets/imgs/cards/7_of_clubs.png" selected-card-id="21" onClick="print_id(21)">', false];
+dict['22'] = ['<img class="selected-cards" src="assets/imgs/cards/7_of_diamonds.png" selected-card-id="22" onClick="print_id(22)" style="color: red; ">', false];
+dict['23'] = ['<img class="selected-cards" src="assets/imgs/cards/7_of_hearts.png" selected-card-id="23" onClick="print_id(23)" style="color: red; ">', false];
+dict['24'] = ['<img class="selected-cards" src="assets/imgs/cards/7_of_spades.png" selected-card-id="24" onClick="print_id(24)">', false];
+dict['25'] = ['<img class="selected-cards" src="assets/imgs/cards/8_of_clubs.png" selected-card-id="25" onClick="print_id(25)">', false];
+dict['26'] = ['<img class="selected-cards" src="assets/imgs/cards/8_of_diamonds.png" selected-card-id="26" onClick="print_id(26)" style="color: red; ">', false];
+dict['27'] = ['<img class="selected-cards" src="assets/imgs/cards/8_of_hearts.png" selected-card-id="27" onClick="print_id(27)" style="color: red; ">', false];
+dict['28'] = ['<img class="selected-cards" src="assets/imgs/cards/8_of_spades.png" selected-card-id="28" onClick="print_id(28)">', false];
+dict['29'] = ['<img class="selected-cards" src="assets/imgs/cards/9_of_clubs.png" selected-card-id="29" onClick="print_id(29)">', false];
+dict['30'] = ['<img class="selected-cards" src="assets/imgs/cards/9_of_diamonds.png" selected-card-id="30" onClick="print_id(30)" style="color: red; ">', false];
+dict['31'] = ['<img class="selected-cards" src="assets/imgs/cards/9_of_hearts.png" selected-card-id="31" onClick="print_id(31)" style="color: red; ">', false];
+dict['32'] = ['<img class="selected-cards" src="assets/imgs/cards/9_of_spades.png" selected-card-id="32" onClick="print_id(32)">', false];
+dict['33'] = ['<img class="selected-cards" src="assets/imgs/cards/10_of_clubs.png" selected-card-id="33" onClick="print_id(33)">', false];
+dict['34'] = ['<img class="selected-cards" src="assets/imgs/cards/10_of_diamonds.png" selected-card-id="34" onClick="print_id(34)" style="color: red; ">', false];
+dict['35'] = ['<img class="selected-cards" src="assets/imgs/cards/10_of_hearts.png" selected-card-id="35" onClick="print_id(35)" style="color: red; ">', false];
+dict['36'] = ['<img class="selected-cards" src="assets/imgs/cards/10_of_spades.png" selected-card-id="36" onClick="print_id(36)">', false];
+dict['37'] = ['<img class="selected-cards" src="assets/imgs/cards/jack_of_clubs2.png" selected-card-id="37" onClick="print_id(37)">', false];
+dict['38'] = ['<img class="selected-cards" src="assets/imgs/cards/jack_of_diamonds2.png" selected-card-id="38" onClick="print_id(38)" style="color: red; ">', false];
+dict['39'] = ['<img class="selected-cards" src="assets/imgs/cards/jack_of_hearts2.png" selected-card-id="39" onClick="print_id(39)" style="color: red; ">', false];
+dict['40'] = ['<img class="selected-cards" src="assets/imgs/cards/jack_of_spades2.png" selected-card-id="40" onClick="print_id(40)">', false];
+dict['41'] = ['<img class="selected-cards" src="assets/imgs/cards/queen_of_clubs2.png" selected-card-id="41" onClick="print_id(41)">', false];
+dict['42'] = ['<img class="selected-cards" src="assets/imgs/cards/queen_of_diamonds2.png" selected-card-id="42" onClick="print_id(42)" style="color: red; ">', false];
+dict['43'] = ['<img class="selected-cards" src="assets/imgs/cards/queen_of_hearts2.png" selected-card-id="43" onClick="print_id(43)" style="color: red; ">', false];
+dict['44'] = ['<img class="selected-cards" src="assets/imgs/cards/queen_of_spades2.png" selected-card-id="44" onClick="print_id(44)">', false];
+dict['45'] = ['<img class="selected-cards" src="assets/imgs/cards/king_of_clubs2.png" selected-card-id="45" onClick="print_id(45)">', false];
+dict['46'] = ['<img class="selected-cards" src="assets/imgs/cards/king_of_diamonds2.png" selected-card-id="46" onClick="print_id(46)" style="color: red; ">', false];
+dict['47'] = ['<img class="selected-cards" src="assets/imgs/cards/king_of_hearts2.png" selected-card-id="47" onClick="print_id(47)" style="color: red; ">', false];
+dict['48'] = ['<img class="selected-cards" src="assets/imgs/cards/king_of_spades2.png" selected-card-id="48" onClick="print_id(48)">', false];
+dict['49'] = ['<img class="selected-cards" src="assets/imgs/cards/ace_of_clubs.png" selected-card-id="49" onClick="print_id(49)">', false];
+dict['50'] = ['<img class="selected-cards" src="assets/imgs/cards/ace_of_diamonds.png" selected-card-id="50" onClick="print_id(50)" style="color: red; ">', false];
+dict['51'] = ['<img class="selected-cards" src="assets/imgs/cards/ace_of_hearts.png" selected-card-id="51" onClick="print_id(51)" style="color: red; ">', false];
+dict['52'] = ['<img class="selected-cards" src="assets/imgs/cards/ace_of_spades.png" selected-card-id="52" onClick="print_id(52)">', false];
 
 var myArray = [];
 
 
 function print_id(id){
-  if(myArray.length < 4 && dict[id][1 ] == false){
+  if(myArray.length < 4 && dict[id][1] == false){
 
       dict[id][1] = true;
       myArray.push(id);
       $( "#append" ).append( dict[id][0] );
+      console.log('works')
+      console.log(id)
       let mainCard = document.querySelectorAll('[card-id="'+id+'"]')[0].lastElementChild;
       let mainCardClone = document.querySelectorAll('[card-id="'+id+'"]')[1].lastElementChild;
       mainCard.style.opacity = "0.3"
@@ -69,8 +71,7 @@ function print_id(id){
       //card.classList.add("selectedCard")
       //cardCloned.classList.add("selectedCard")
   } else if(myArray.length > 0 && dict[id][1] == true) {
-    console.log('works')
-    console.log(id)
+
     dict[id][1] = false;
     for (let i = 0; i <= myArray.length; i++) {
       if (myArray[i] == id) {
@@ -78,7 +79,9 @@ function print_id(id){
         break;
       }
     }
-    $( "#append" ).remove( dict[id][1] )
+    // $( "#append" ).remove( '[selected-card-id="'+id+'"]' )
+    var element = document.querySelectorAll('[selected-card-id="'+id+'"]')[0];
+    element.parentNode.removeChild(element)
     let mainCard = document.querySelectorAll('[card-id="'+id+'"]')[0].lastElementChild;
     let mainCardClone = document.querySelectorAll('[card-id="'+id+'"]')[1].lastElementChild;
     mainCard.style.opacity = "1"
